0dd85d0955713cb0be02b34664d2f2eb6f6b90b7
SimonIsNotAvailable <polushkinsn@gmail.com>
2022-08-20 15:37:53 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] input = {1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
+        List<Integer> list = new ArrayList<>();
+        for (int x : input) {
+            if (x > 0 & x % 2 == 0) {
+                list.add(x);
+            }
+        }
+        list.sort(Comparator.naturalOrder());
+        for (int result : list) {
+            System.out.println(result);
+        }
+    }
+}
